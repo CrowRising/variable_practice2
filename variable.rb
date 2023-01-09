ddd20175f2f8f67d7c5c128e12e04872e499820d
Crow Rising <miscbyrd@gmail.com>
2023-01-09 11:24:24 -0700

    Change variables and run puts

--- variable.rb
+++ variable.rb
@@ -1,16 +1,40 @@
 # Adding a variable that is assigned to strings
 names = ["george", "hank", "camielle", "rebecca", "frances"]
+puts names
+names = [ "Dracula"]
+puts  names
+
+
 
 # Add variables assigned to integers
 numbers = 2, 544, 91, 30, 57
+puts numbers
+numbers = 33, 21, 44, 0, 9
+puts numbers
 
 # Add variables that are addisgned to floats
 deposit_paid = 1450.00
 checked_in = 11.3
+puts deposit_paid 
+puts checked_in
+
+deposit_paid = 700.00
+checked_in = 3.5
+puts checked_in
+puts deposit_paid
 
 # Add variables that are assigned to boolens"
 attended = false
 rescheduled = true
+ puts attended
+ puts rescheduled
+
+ attended = true
+ rescheduled = false
+ puts attended
+ puts rescheduled
+ 
+
 
 
 
