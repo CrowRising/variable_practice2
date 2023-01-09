114df0f3e873608d34b37aa2adffc83977ee2c4e
Crow Rising <miscbyrd@gmail.com>
2023-01-09 10:55:00 -0700

    Add variables assigned to integers

--- variable.rb
+++ variable.rb
@@ -1,1 +1,6 @@
+# Adding a variable that is assigned to strings
 names = ["george", "hank", "camielle", "rebecca", "frances"]
+
+# Add variables assigned to integers
+numbers = [2, 544, 91, 30, 57]
+
